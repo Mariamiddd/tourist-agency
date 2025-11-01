f13459c8f664f80f04e846e1e3994278ee21ba78
Your Name <you@example.com>
2025-11-01 22:37:00 +0400

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('toggle');
+});
+
+
 let slideIndex = 1;
-showSlides(slideIndex); 
+showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
-} 
+}
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
@@ -14,15 +22,15 @@ function showSlides(n) {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
